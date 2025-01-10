45f80826f6ced1546a6c53bf9a45f77163ec019a
Sena <senalikescake@gmail.com>
2025-01-10 15:56:42 +0000

    Grito cria hitbox q deteta outras hitboxes. NullReferenceException ao procurar pelo componente PlayerCombat dos hits

--- Assets/Scripts/Grito.cs
+++ Assets/Scripts/Grito.cs
@@ -1,29 +1,66 @@
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Grito : MonoBehaviour
 {
-    public CircleCollider2D colliderGrito;
     public bool canAttack = true;
     public int player;
     public GameObject grito;
+    public CircleCollider2D colliderGrito;
+    public GameObject instanciaGrito;
+    public bool isGrito = false;
+    public PlayerMovement movement;
+
+    void Start(){
+        movement = GetComponent<PlayerMovement>();
+    }
+
     void Update()
     {
         //Debug.Log(isAttacking);
-		if (player == 1){
-			if (Input.GetKeyDown(KeyCode.I) && canAttack)
-			{
-				AttackGrito();
-			}
-			else
-			{
-				//Disable animator
-			}   
-		}
-    }
-    void AttackGrito(){
-        Instantiate(grito, transform.position, Quaternion.identity);
-        while (Input.GetKey(KeyCode.I)){
+		if (player == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.I) && canAttack)
+            {
+                AttackGrito();
+            }
+            else if (Input.GetKeyUp(KeyCode.I) && instanciaGrito != null)
+            {
+                StopGrito();
+            }
 
+            if (instanciaGrito != null){
+                Collider2D[] hits = Physics2D.OverlapCircleAll(grito.transform.position, colliderGrito.radius);
+                foreach (Collider2D hit in hits)
+                {
+                    Debug.Log("Detect");
+                    if (hit.tag == "Player" && hit.GetComponent<PlayerCombat>().player != this.player)
+                    {
+                        Debug.Log("Hit");
+                        PlayerHealth enemyHealth = hit.transform.root.GetComponent<PlayerHealth>();
+                        if (movement.isFacingRight)
+                            enemyHealth.TakeDamage(20, 5f);
+                        else
+                            enemyHealth.TakeDamage(10, -5f);
+                    }
+                }
+            }
+            
         }
     }
-}
+    void AttackGrito()
+        {
+            instanciaGrito = Instantiate(grito, transform.position, Quaternion.identity);
+            colliderGrito = instanciaGrito.GetComponent<CircleCollider2D>();
+            Debug.Log("Grito started");
+            isGrito = true;
+        }
+
+    void StopGrito()
+        {
+            Debug.Log("Grito stopped");
+            Destroy(instanciaGrito);
+            isGrito = false;
+        }
+    }
